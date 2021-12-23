fec94ffe41cd51749a8c86dd1f149f41ffa73493
Andrew Henry <andrewmhenry@me.com>
2021-12-23 14:46:09 +0800

    ➕ feat(Rinkeby Networm Error): updated message

--- src/App.jsx
+++ src/App.jsx
@@ -177,6 +177,7 @@ const App = () => {
       <div className='unsupported-network'>
         <h2>Please connect to Rinkeby</h2>
         <p>This dapp only works on the Rinkeby network, please switch networks in your connected wallet.</p>
+        <h1>🌶️</h1>
       </div>
     )
   }
