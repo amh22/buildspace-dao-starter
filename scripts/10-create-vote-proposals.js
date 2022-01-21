94dc27cd0d5ec78b9f13395017bac304e736bf0e
Andrew Henry <andrewmhenry@me.com>
2022-01-21 11:58:50 +0800

    🚀 feat(Gov Proposals): updated the proposals

--- scripts/10-create-vote-proposals.js
+++ scripts/10-create-vote-proposals.js
@@ -9,24 +9,29 @@ const tokenModule = sdk.getTokenModule('0xA0771B18898c090F8f563b4F0955A89a4e6DC5
 
 ;(async () => {
   try {
-    const amount = 420_000
+    const amount = 120_000
     // Create proposal to mint 420,000 new token to the treasury.
-    await voteModule.propose('Should the DAO mint an additional ' + amount + ' tokens into the treasury?', [
-      {
-        // Our nativeToken is ETH. nativeTokenValue is the amount of ETH we want
-        // to send in this proposal. In this case, we're sending 0 ETH.
-        // We're just minting new tokens to the treasury. So, set to 0.
-        nativeTokenValue: 0,
-        transactionData: tokenModule.contract.interface.encodeFunctionData(
-          // We're doing a mint! And, we're minting to the voteModule, which is
-          // acting as our treasruy.
-          'mint',
-          [voteModule.address, ethers.utils.parseUnits(amount.toString(), 18)]
-        ),
-        // Our token module that actually executes the mint.
-        toAddress: tokenModule.address,
-      },
-    ])
+    await voteModule.propose(
+      'Should the DAO mint an additional ' +
+        amount +
+        ' tokens into the treasury for rewarding contributors to the community?',
+      [
+        {
+          // Our nativeToken is ETH. nativeTokenValue is the amount of ETH we want
+          // to send in this proposal. In this case, we're sending 0 ETH.
+          // We're just minting new tokens to the treasury. So, set to 0.
+          nativeTokenValue: 0,
+          transactionData: tokenModule.contract.interface.encodeFunctionData(
+            // We're doing a mint! And, we're minting to the voteModule, which is
+            // acting as our treasruy.
+            'mint',
+            [voteModule.address, ethers.utils.parseUnits(amount.toString(), 18)]
+          ),
+          // Our token module that actually executes the mint.
+          toAddress: tokenModule.address,
+        },
+      ]
+    )
 
     console.log('✅ Successfully created proposal to mint tokens')
   } catch (error) {
@@ -35,14 +40,14 @@ const tokenModule = sdk.getTokenModule('0xA0771B18898c090F8f563b4F0955A89a4e6DC5
   }
 
   try {
-    const amount = 6_900
+    const amount = 2_000
     // Create proposal to transfer ourselves 6,900 token for being awesome.
     await voteModule.propose(
       'Should the DAO transfer ' +
         amount +
         ' tokens from the treasury to ' +
         process.env.WALLET_ADDRESS +
-        ' for being awesome?',
+        ' for their contribution to Diversity Guild?',
       [
         {
           // Again, we're sending ourselves 0 ETH. Just sending our own token.
